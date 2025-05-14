a7fc4eac8e6d5a59d17e5769ed26c50120145d82
amarjupalli <bittuamar@gmail.com>
2025-05-13 22:09:38 -0700

    fix(handleSubmit): read form values and log

--- src/app/modules/formBuilder/handleSubmit.ts
+++ src/app/modules/formBuilder/handleSubmit.ts
@@ -1,9 +1,9 @@
 function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
   e.preventDefault();
-  console.log(
-    "On Save, formValues:",
-    new FormData(e.target as HTMLFormElement),
-  );
+  const rawFormData = new FormData(e.target as HTMLFormElement);
+  const formValues = Object.fromEntries(rawFormData);
+
+  console.log("formValues", formValues);
 }
 
 export default handleSubmit;
